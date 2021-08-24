6246d59977fe3a92b00f04c1898c1c5495dd9f0f
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-08-24 16:00:47 +0200

    axios

--- src/components/ListCharacter.vue
+++ src/components/ListCharacter.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="listCharacter"></div>
+  <div class="listCharacter">
+    <div v-for="(item, index) in listCocktails" :key="index"></div>
+  </div>
 </template>
 
 <script>
@@ -8,13 +10,36 @@ import axios from "axios";
 
 export default {
   name: "ListCharacter",
-},
-data(){
-    return{
-        apiURL:'https://flynn.boolean.careers/exercises/api/array/music',
-        listCocktails: [],
-    }
-}
+
+  data() {
+    return {
+      apiKey: "1",
+      apiURL:
+        "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=martini",
+      listCocktails: [],
+      searchCocktail: "martini",
+    };
+  },
+  created() {
+    this.getCocktails();
+  },
+  methods: {
+    getCocktails() {
+      const request = {
+        params: {
+          api_key: this.apiKey,
+          query: this.searchCocktail,
+        },
+      };
+
+      axios.get(this.apiURL, request).then((res) => {
+        console.log(res);
+        this.listCocktails = res.data;
+        console.log(this.listCocktails);
+      });
+    },
+  },
+};
 </script>
 
 <style lang='scss' scoped>
